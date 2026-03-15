b1b9593bd5e41d7dd8ef0a2270dd946a5494ebd0
KING-JAXXTY <ANDREWCADITAN6@GMAIL.COM>
2026-03-16 00:22:39 +0800

    v124.8 - Fixed duplicate show password icons by hiding browser's built-in password reveal controls

--- src/App.tsx
+++ src/App.tsx
@@ -1679,7 +1679,7 @@ function App() {
                 fontSize: '0.75rem',
                 fontWeight: '400'
               }}>
-                Version 124.7
+                Version 124.8
               </div>
 
             </div>
